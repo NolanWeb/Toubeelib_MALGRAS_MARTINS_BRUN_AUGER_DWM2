a212a093f0410f47a9137705c6cae694c2ef3a8f
felixbrun0 <felix.brun6@etu.univ-lorraine.fr>
2024-09-10 17:08:17 +0200

    ajout rdv

---
+++ app/src/core/domain/entities/rdv/Rdv.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace toubeelib\core\domain\entities\rdv;
+
+use DateTimeImmutable;
+use toubeelib\core\domain\entities\Entity;
+use toubeelib\core\domain\entities\patient\Patient;
+use toubeelib\core\domain\entities\praticien\Praticien;
+use toubeelib\core\dto\RdvDTO;
+
+class Rdv extends Entity
+{
+    protected DateTimeImmutable $date;
+    protected int $duree;
+    protected Praticien $praticien;
+    protected Patient $patient;
+    protected string $statut;
+
+    public function __construct(DateTimeImmutable $date, int $duree, Praticien $praticien, Patient $patient, string $statut)
+    {
+        $this->date = $date;
+        $this->duree = $duree;
+        $this->praticien = $praticien;
+        $this->patient = $patient;
+        $this->statut = $statut;
+    }
+
+    public function toDTO(): RdvDTO
+    {
+        return new RdvDTO($this);
+    }
+}
---
+++ app/src/core/dto/RdvDTO.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace toubeelib\core\dto;
+
+use DateTimeImmutable;
+use toubeelib\core\domain\entities\patient\Patient;
+use toubeelib\core\domain\entities\praticien\Praticien;
+use toubeelib\core\domain\entities\rdv\Rdv;
+use toubeelib\core\dto\DTO;
+
+class RdvDTO extends DTO
+{
+    protected string $ID;
+    protected DateTimeImmutable $date;
+    protected Praticien $praticien;
+    protected Patient $patient;
+    protected string $statut;
+
+
+    public function __construct(Rdv $rdv)
+    {
+        $this->ID = $rdv->getID();
+        $this->date = $rdv->date;
+        $this->praticien = $rdv->praticien;
+        $this->patient = $rdv->patient;
+        $this->statut = $rdv->statut;
+    }
+
+
+}
