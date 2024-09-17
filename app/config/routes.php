7a908abdaf3cacb90e40c378a82629bfdc8f818d
Nolan Malgras Martins <nolan.malgras-martins1@etu.univ-lorraine.fr>
2024-09-17 17:08:44 +0200

    Dans routes.php : • accéder à un rendez-vous • créer un rendez-vous • modifier un rendez-vous --> OK

--- app/config/routes.php
+++ app/config/routes.php
@@ -8,22 +8,12 @@ return function( \Slim\App $app):\Slim\App {
 
     $app->get('/',
         \toubeelib\application\actions\HomeAction::class);
-    $app->post('/api/commandes/add',
-        \toubeelib\application\actions\CreateCommandeAction::class);
-    $app->post('/api/commandes/{commandeId}/items',
-        \toubeelib\application\actions\AddItemToCommandeAction::class);
-    $app->put('/api/commandes/{commandeId}/client',
-        \toubeelib\application\actions\SetClientForCommandeAction::class);
-    $app->get('/api/clients/{clientId}/commandes',
-        \toubeelib\application\actions\ListCommandesForClientAction::class);
-    $app->get('/api/commandes/{commandeId}/items',
-        \toubeelib\application\actions\ListItemsForCommandeAction::class);
-    $app->get('/commandes/{id}',
-        \toubeelib\application\actions\GetCommandeAction::class);
-    $app->put('/commande/set-paiement/{id}',
-        \toubeelib\application\actions\SetPaiementAction::class);
-    $app->get('/commande/get_client/{id}',
-        \toubeelib\application\actions\GetClientForCommandeAction::class);
+    $app->get('/rdv/{id}',
+        \toubeelib\application\actions\GetRdvAction::class);
+    $app->post('/rdv/create',
+        \toubeelib\application\actions\CreateRdvAction::class);
+    $app->put('/rdv/{id}/update',
+        \toubeelib\application\actions\UpdateRdvAction::class);
 
     return $app;
 };
