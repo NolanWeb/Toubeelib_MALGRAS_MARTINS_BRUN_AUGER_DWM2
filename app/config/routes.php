929ffdc464652f3c84a210fbe1c0b06f507c4a57
Nolan Malgras Martins <nolan.malgras-martins1@etu.univ-lorraine.fr>
2024-09-17 17:03:15 +0200

    routes.php --> OK

--- app/config/routes.php
+++ app/config/routes.php
@@ -6,8 +6,24 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 return function( \Slim\App $app):\Slim\App {
 
-    $app->get('/', \toubeelib\application\actions\HomeAction::class);
-
+    $app->get('/',
+        \toubeelib\application\actions\HomeAction::class);
+    $app->post('/api/commandes/add',
+        \toubeelib\application\actions\CreateCommandeAction::class);
+    $app->post('/api/commandes/{commandeId}/items',
+        \toubeelib\application\actions\AddItemToCommandeAction::class);
+    $app->put('/api/commandes/{commandeId}/client',
+        \toubeelib\application\actions\SetClientForCommandeAction::class);
+    $app->get('/api/clients/{clientId}/commandes',
+        \toubeelib\application\actions\ListCommandesForClientAction::class);
+    $app->get('/api/commandes/{commandeId}/items',
+        \toubeelib\application\actions\ListItemsForCommandeAction::class);
+    $app->get('/commandes/{id}',
+        \toubeelib\application\actions\GetCommandeAction::class);
+    $app->put('/commande/set-paiement/{id}',
+        \toubeelib\application\actions\SetPaiementAction::class);
+    $app->get('/commande/get_client/{id}',
+        \toubeelib\application\actions\GetClientForCommandeAction::class);
 
     return $app;
 };
