0c91cb0c22358206bc8719a3805f75c393a7d943
bnj35 <benjamin.auger6@etu.univ-lorraine.fr>
2024-10-08 17:56:39 +0200

    patch cors

--- app/config/routes.php
+++ app/config/routes.php
@@ -7,13 +7,11 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 return function( \Slim\App $app):\Slim\App {
 
-    $app->add(\toubeelib\application\middlewares\Cors::class );
+    $app->add(\toubeelib\application\middlewares\Cors::class);
 
-    $app->options('/{routes:.+}',
-        function( Request $rq,
-                  Response $rs, array $args) : Response {
-            return $rs;
-        });
+    $app->options('/{routes:.+}', function (Request $rq, Response $rs, array $args): Response {
+        return $rs;
+    });
 
     $app->get('/',
         \toubeelib\application\actions\HomeAction::class);
@@ -22,18 +20,18 @@ return function( \Slim\App $app):\Slim\App {
         \toubeelib\application\actions\GetAllRdvsAction::class)
         ->setName('getAllRdvs');
 
-    $app->get('/rdv/{id}[/]',
+    $app->get('/rdvs/{id}[/]',
         \toubeelib\application\actions\GetRdvAction::class)
         ->setName('getRdv');
 
-    $app->post('/rdv/create',
+    $app->post('/rdvs/create',
         \toubeelib\application\actions\CreateRdvAction::class);
 
-    $app->patch('/rdv/{id}',
+    $app->patch('/rdvs/{id}',
         \toubeelib\application\actions\UpdateRdvAction::class)
         ->setName('updateRdv');
         
-    $app->delete('/rdv/{id}',
+    $app->delete('/rdvs/{id}',
         \toubeelib\application\actions\DeleteRdvAction::class)
         ->setName('deleteRdv');
 
--- app/src/application/middlewares/Cors.php
+++ app/src/application/middlewares/Cors.php
@@ -2,24 +2,18 @@
 
 namespace toubeelib\application\middlewares;
 
-use PhpParser\Node\Expr\New_;
-use Slim\Exception\HttpUnauthorizedException;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 class Cors
 {
-
-    public function __invoke($request, $response, $next)
+    public function __invoke(Request $request, RequestHandler $handler): Response
     {
-        if (! $request->hasHeader('Origin')) {
-            New HttpUnauthorizedException($request, 'Origin header is required');
-        }
-        $response = $next->handle($request);
+        $response = $handler->handle($request);
         return $response
-            ->withHeader('Access-Control-Allow-Origin', $request->getHeader('Origin'))
-            ->withHeader('Access-Control-Allow-Methods', 'POST, PUT, GET')
-            ->withHeader('Access-Control-Allow-Headers', 'Authorization')
-            ->withHeader('Access-Control-Max-Age', '3600')
-            ->withHeader('Access-Control-Allow-Credentials', 'true');
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     }
-
 }
